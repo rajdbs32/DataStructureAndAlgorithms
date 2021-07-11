f88bfe31779b9b41107ae47e05c95c7888eb19f7
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-07-12 00:08:51 +0530

    Simple interest calculator

--- simpleInterestProgramme/SimpleInterestCalculator.java
+++ simpleInterestProgramme/SimpleInterestCalculator.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class SimpleInterestCalculator {
 
 	public static void main(String[] args) {
-		
+		//Simple interest calculattor
 		Scanner sc = new Scanner(System.in);
 		int PrincipalAmount = sc.nextInt();
 		float rate = sc.nextFloat();
 		int time = sc.nextInt();
 		// formula for si = si*rste*time/100
 		float simpleInterest = PrincipalAmount * rate * time / 100;
-		
+
 		System.out.println("the simple interest is " + simpleInterest);
-		
+
 	}
 
 }
