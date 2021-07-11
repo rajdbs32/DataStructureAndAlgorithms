7d6d536150b5c35178726c22c82b971597c53af0
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-07-11 22:55:16 +0530

    Usage of ternary operators

---
+++ nestedIfElse/TernaryOperatorNested.java
@@ -0,0 +1,17 @@
+package nestedIfElse;
+import java.util.Scanner;
+public class TernaryOperatorNested {
+
+	public static void main(String[] args) {
+		//Variable declaration first 
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int result = 0;
+		
+		result = a>b ? a>c ? a : c : b>c ? b : c;
+		System.out.println("the greatest number is " +result);
+	}
+	
+}
