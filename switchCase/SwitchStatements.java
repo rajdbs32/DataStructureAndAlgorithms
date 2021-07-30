b5855797c6b79b5971bafc6797e527160c18f612
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-07-30 21:34:13 +0530

    More convenient than If else

---
+++ switchCase/SwitchStatements.java
@@ -0,0 +1,48 @@
+package switchCase;
+
+public class SwitchStatements {
+
+    public static void main(String[] args){
+        //switch is an alternative for if else yet more simple to write
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
