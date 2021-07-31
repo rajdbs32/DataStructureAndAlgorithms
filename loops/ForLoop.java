d568b2bc64a9f0be25aef13373e3ea22a2b13940
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-07-31 21:23:57 +0530

    For loop Basic Syntax

---
+++ loops/ForLoop.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class ForLoop {
+
+    public static void main(String[] args){
+            //to find the sum of all numbers from 1 to 100 or upto n number.
+        int n = 100; //or n times
+        int sum = 0;
+        for (int i = 1; i <= n; i++ ) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+
+
+
+}
+
+
