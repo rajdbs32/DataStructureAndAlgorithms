1a2ebf6b00ce6a46dfc47693e55e2ab1490b3aa1
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-07-31 21:22:31 +0530

    Multiplication table based on For loop

---
+++ loops/MultiplicationTableForloop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class MultiplicationTableForloop {
+
+    public static void main(String[] args){
+
+        int tableOf = 4; // the number of table to be made
+        for(int i = 1 ; i <=10; i++){
+            System.out.println(tableOf * i);
+        }
+
+
+    }
+
+}
