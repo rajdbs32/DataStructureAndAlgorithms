2bab6969b436de47574cb55110c3c4851403594f
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-02 20:25:00 +0530

    Programme to check if prime number.

---
+++ loops/PrimeNumberChecker.java
@@ -0,0 +1,21 @@
+package loops;
+
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for( int i = 2 ; i<n ; i++){
+            if (n % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if (n < 2) isPrime = false;
+    System.out.println ("isprime " + isPrime);
+
+
+    }
+}
