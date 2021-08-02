777d44bbb723a7f970aec17a863ff28c74ab14af
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-02 21:27:06 +0530

    Use of nested loops

--- loops/NestedLoops.java
+++ loops/NestedLoops.java
@@ -2,8 +2,8 @@ package loops;
 
 public class NestedLoops {
     public static void main(String[] args){
-        for(int j=1; j<=5; j++){
-            for(int i=1 ; i<=10; i++) {
+        for(int j=1; j<=5; j++){ //this for will run the inner loop j times
+            for(int i=1 ; i<=10; i++) {   //for loop to print numbers
                 System.out.print(i);
             }
             System.out.println();
