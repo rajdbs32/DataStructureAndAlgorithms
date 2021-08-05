9e3d418aa2fc48d5dde3b24b8b63da35494dc1e2
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 21:55:54 +0530

    Update PatternMAking2.java

--- loops/PatternMAking2.java
+++ loops/PatternMAking2.java
@@ -6,12 +6,12 @@ public class PatternMAking2 {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
         int n= sc.nextInt();
-        for(int i= 1; i<=n; i++){
-            for(int j = 1; j<=n; j++) {
+        for(int i= 1; i<=n; i++){  //outer loop for the rows
+            for(int j = 1; j<=n; j++) {       //inner loop for the pattern.
                 if (j >= i) {
                     System.out.print("* ");
                 } else {
-                    System.out.print("  ");
+                    System.out.print("  "); //to print out he spaces
 
                 }
             }
