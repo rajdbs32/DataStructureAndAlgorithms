ab23aa41fdbe12e18b5f63e03158cfc14485264f
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 20:37:41 +0530

    While loops

---
+++ loops/WhileLoops.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class WhileLoops {
+    public static void main(String[] args){
+        int i=0; //INITIALISATION
+        while(i<100){
+            System.out.println(i);   //CODE TO BE EXECUTED
+            i++;                   //ITERATION
+        }
+    }
+}
