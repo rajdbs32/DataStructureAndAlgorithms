569ad33c6f1d8b913f8298317e62c260583ddc48
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 20:39:06 +0530

    While loops

--- loops/WhileLoops.java
+++ loops/WhileLoops.java
@@ -3,9 +3,9 @@ package loops;
 public class WhileLoops {
     public static void main(String[] args){
         int i=0; //INITIALISATION
-        while(i<100){
-            System.out.println(i);   //CODE TO BE EXECUTED
-            i++;                   //ITERATION
+        while(i<100){  //CONDITION
+            System.out.println(i);   //CODE TO BE EXECUTED UNTIL CONDITION TRUE.
+            i++;              //ITERATION
         }
     }
 }
