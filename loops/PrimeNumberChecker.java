5b9750ea6ae3a658540b83409afae6820692d0c3
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 21:54:31 +0530

    Update PrimeNumberChecker.java

--- loops/PrimeNumberChecker.java
+++ loops/PrimeNumberChecker.java
@@ -1,13 +1,13 @@
 package loops;
 
-import java.util.Scanner;
+import java.util.Scanner; //needs to be imported manually to use the Scanner function.
 
 public class PrimeNumberChecker {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //takes input from the user.
         int n = sc.nextInt();
         boolean isPrime = true;
-        for( int i = 2 ; i<n ; i++){
+        for( int i = 2 ; i<n ; i++){     //(init:condition:iteration)      
             if (n % i == 0){
                 isPrime = false;
                 break;
@@ -15,7 +15,5 @@ public class PrimeNumberChecker {
         }
         if (n < 2) isPrime = false;
     System.out.println ("isprime " + isPrime);
-
-
-    }
+  }
 }
