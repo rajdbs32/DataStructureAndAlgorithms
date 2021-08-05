527906954803c0abe5836dbeea1f73a6d9d13958
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 20:47:38 +0530

    Do while loop

--- loops/DoWhileLoop.java
+++ loops/DoWhileLoop.java
@@ -2,12 +2,11 @@ package loops;
 
 public class DoWhileLoop {
     public static void main(String[] args){
-        int i=0;
+        int i=0; //INITIALISATION
          do {
-             System.out.println(i);
+             System.out.println(i); //this will be printed no-matter what is the condition
              i++;
-         }while(i>100);
-
-         }
+         }while(i>100); //condition
+    }
 }
 
