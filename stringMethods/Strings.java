bf4a7ece7834906c215c9b746f678d0112a647e0
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-05 21:00:05 +0530

    USe of StringMethod

---
+++ stringMethods/Strings.java
@@ -0,0 +1,12 @@
+package stringMethods;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Rajdeep";
+        String channel = "Data structures and algorithms";
+        System.out.print(name.length()); //tells us the no of letters in the variable name.
+        System.out.println(name.toLowerCase());// converts name to lowercase.
+        System.out.println(name + " from " + channel); //CONCATENATED
+
+    }
+}
