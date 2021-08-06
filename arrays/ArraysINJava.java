ca4b70e6239dfeb2ff9c75a436639d7398e949fc
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-06 20:06:33 +0530

    Introduction to ArraysINJava

---
+++ arrays/ArraysINJava.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class ArraysINJava {
+    public static void main(String[] args){
+        //arrays syntax and use.
+        int [] marks = {1,2,3,5};
+        marks [3]=34;   //updated the value aat 3rd position from 5 to 34.
+        System.out.println(marks[0]);  //value at 0th position i.e. 1.
+        System.out.println(marks[3]); //prints the value at 3rd position.
+
+        //for loop in arrays.
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]); //prints all the values in the array marks
+        }
+
+    }
+}
